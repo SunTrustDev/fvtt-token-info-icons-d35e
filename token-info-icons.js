20b7c30b0de130cccb42ec5d044daa6204ad2c74
Simone Romano <sromano@tierratelematics.com>
2024-01-26 14:12:29 +0100

    Bump id

--- token-info-icons.js
+++ token-info-icons.js
@@ -1,4 +1,38 @@
 import { TokenInfoIcons35E } from "./d35e";
+class TokenInfoIcons35E {
+    static async addTokenInfoButtons(app, html, data) {
+        let actor = canvas.tokens.get(data._id).actor;
+        if (actor === undefined) return;
+
+        let ac = actor.system.attributes.ac.normal.total;
+
+        let speed = '<span class="token-info-speed" title="Land"><i class="fas fa-walking"></i> ' + actor.system.attributes.speed.land.total + '</span>';
+
+        let initiative = null;
+        if (actor.data.type === "Player") {
+            initiative = actor.system.attributes.init.total;
+        }
+
+        // D35E-specific buttons
+        let newdiv = '<div class="token-info-container">';
+        let position = game.settings.get('token-info-icons', 'position');
+
+        const speedButton = `<div class="control-icon token-info-icon" title="Speed">${speed}</div>`;
+        const acButton = `<div class="control-icon token-info-icon" title="Armor Class: ${ac}"><i class="fas fa-shield-alt"></i> ${ac}</div>`;
+        
+        let defaultButtons = `${speedButton}${acButton}`;
+
+        if (initiative != null) {
+            const initiativeButton = `<div class="control-icon token-info-icon" title="Initiative: ${initiative}"><i class="fas fa-bolt"></i> ${initiative}</div>`;
+            defaultButtons += initiativeButton;
+        }
+
+        let buttons = $(`<div class="col token-info-column-${position}">${defaultButtons}</div>`);
+
+        html.find('.col.left').wrap(newdiv);
+        html.find('.col.left').before(buttons);
+    }
+}
 
 class TokenInfoIcons {
     static async addTokenInfoButtons(app, html, data) {
@@ -144,7 +178,7 @@ Hooks.on('ready', () => {
 
 Hooks.once("init", () => {
 
-    game.settings.register('token-info-icons', 'gmOnly', {
+    game.settings.register('token-info-icons-d35e', 'gmOnly', {
         name: "GM only?",
         hint: "Show the token info to the GM only or to all players?",
         scope: "world",
@@ -153,7 +187,7 @@ Hooks.once("init", () => {
         type: Boolean
     });
 
-    game.settings.register('token-info-icons', 'allPassiveSenses', {
+    game.settings.register('token-info-icons-d35e', 'allPassiveSenses', {
         name: 'Show all passive senses (dnd5e)',
         hint: 'Show passive perception/investigation/insight/stealth instead of just passive perception',
         scope: "world",
@@ -162,7 +196,7 @@ Hooks.once("init", () => {
         type: Boolean
     });
 
-    game.settings.register('token-info-icons', 'position', {
+    game.settings.register('token-info-icons-d35e', 'position', {
         name: "Token Position",
         hint: "Which side of the token should the info appear on?",
         scope: "world",
