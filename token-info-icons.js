8562c40bd3e507c44b27c758364be1395beed366
Simone Romano <sromano@tierratelematics.com>
2024-01-26 18:20:43 +0100

    Refactor

--- token-info-icons.js
+++ token-info-icons.js
@@ -1,4 +1,38 @@
-import { TokenInfoIcons35E } from "./d35e";
+export class TokenInfoIcons35E {
+    static async addTokenInfoButtons(app, html, data) {
+        console.info("Token Info Icons | Adding token info buttons for D35E");
+        let actor = canvas.tokens.get(data._id).actor;
+        if (actor === undefined) return;
+
+        let ac = actor.system.attributes.ac.normal.total;
+
+        let speed = '<span class="token-info-speed" title="Land"><i class="fas fa-walking"></i> ' + actor.system.attributes.speed.land.total + '</span>';
+
+        let initiative = null;
+        if (actor.data.type === "Player") {
+            initiative = actor.system.attributes.init.total;
+        }
+
+        // D35E-specific buttons
+        let newdiv = '<div class="token-info-container">';
+        let position = game.settings.get('token-info-icons-d35e', 'position');
+
+        const speedButton = `<div class="control-icon token-info-icon" title="Speed">${speed}</div>`;
+        const acButton = `<div class="control-icon token-info-icon" title="Armor Class: ${ac}"><i class="fas fa-shield-alt"></i> ${ac}</div>`;
+        
+        let defaultButtons = `${speedButton}${acButton}`;
+
+        if (initiative != null) {
+            const initiativeButton = `<div class="control-icon token-info-icon" title="Initiative: ${initiative}"><i class="fas fa-bolt"></i> ${initiative}</div>`;
+            defaultButtons += initiativeButton;
+        }
+
+        let buttons = $(`<div class="col token-info-column-${position}">${defaultButtons}</div>`);
+
+        html.find('.col.left').wrap(newdiv);
+        html.find('.col.left').before(buttons);
+    }
+}
 
 class TokenInfoIcons {
     static async addTokenInfoButtons(app, html, data) {
